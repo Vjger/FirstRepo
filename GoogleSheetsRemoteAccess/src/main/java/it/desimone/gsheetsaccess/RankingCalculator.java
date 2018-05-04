3a7e370a2a5645c6467ab439893b9c885d37e7ea
mds <mds@marco>
2018-05-04 08:40:20 +0200

    Implementazione calcolo Ranking

---
+++ GoogleSheetsRemoteAccess/src/main/java/it/desimone/gsheetsaccess/RankingCalculator.java
@@ -0,0 +1,29 @@
+package it.desimone.gsheetsaccess;
+
+import it.desimone.gheetsaccess.gsheets.dto.ClassificheRow;
+import it.desimone.gheetsaccess.gsheets.dto.TorneiRow;
+
+import java.util.List;
+
+public class RankingCalculator {
+
+	public static void main(String[] args) {
+		calculate();
+	}
+	
+	public static void calculate(){
+		
+	}
+	
+	private static List<ClassificheRow> getAllClassifiche(){
+		List<ClassificheRow> result = null;
+		
+		return result;
+	}
+	
+	private static List<TorneiRow> getAllTornei(){
+		List<TorneiRow> result = null;
+		
+		return result;
+	}
+}
