4858523a023407ca127fac39a6878e6fe7f64cb9
linda <linda@pc_linda.homenet.telecomitalia.it>
2024-08-15 10:26:42 +0200

    commit delle immagini

---
+++ ReplayRD3/src/main/java/it/desimone/utils/Utils.java
@@ -0,0 +1,9 @@
+package it.desimone.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Utils {
+	public static final DateFormat DATA_INIZIO = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	public static final DateFormat DATA_FINE = new SimpleDateFormat("HH:mm:ss");
+}
